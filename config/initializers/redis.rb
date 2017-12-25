306b08f16b4171447d330f75f3366e08f574a8d2
Ana Paula <anapaula@melo.tech>
2017-12-25 11:54:06 -0300

    fix specs

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
 $redis = Redis::Namespace.new("bookstore", :redis => Redis.new)
-::Domain::Book::Services::Cache::Invalidate.new.call
+::Domain::Book::Services::Cache::Invalidate.new.call unless Rails.env.test?
